c07b9383b87bd5e7ca28d6d327f187b57c5f5534
Arnav Gupta <dev@championswimmer.in>
2024-12-23 01:19:06 +0000

    added BootstrapVueNext - V3, B5
    Signed-off-by: Arnav Gupta <dev@championswimmer.in>

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createBootstrap } from 'bootstrap-vue-next'
 
 import App from './App.vue'
 import router from './router'
+
+// Add bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(createBootstrap())
 
 app.mount('#app')
