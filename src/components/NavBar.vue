d920f213a6625d17ca86eda8f2fe00b07b0ab93b
Arnav Gupta <dev@championswimmer.in>
2024-12-24 02:00:38 +0000

    create bootstrap navbar
    Signed-off-by: Arnav Gupta <dev@championswimmer.in>

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { BNavbar, BNavbarBrand, BNavbarNav, BNavItem } from 'bootstrap-vue-next'
+import { Globe } from 'lucide-vue-next'
+</script>
+
+<template>
+  <BNavbar toggleable="lg">
+    <BNavbarBrand href="#" class="fw-bold">
+      <Globe class="me-2 mb-1" :size="24" />
+      localise.travel
+    </BNavbarBrand>
+    <BNavbarNav>
+      <BNavItem to="/home">Home</BNavItem>
+      <BNavItem to="/about">About</BNavItem>
+      <BNavItem to="/contact">Contact</BNavItem>
+    </BNavbarNav>
+  </BNavbar>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createBootstrap } from 'bootstrap-vue-next'
@@ -10,6 +8,9 @@ import router from './router'
 // Add bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+// Add our CSS
+import './assets/main.css'
 
 const app = createApp(App)
 
