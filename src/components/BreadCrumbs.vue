e0b7503ff08b6cfb1f457655195badaca78ae2f7
Akshaygore1 <akshaygore2301@gmail.com>
2024-12-28 01:53:36 +0530

    Added Breadcrumbs

---
+++ src/components/BreadCrumbs.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { type BreadcrumbItem } from 'bootstrap-vue-next'
+import { ref } from 'vue'
+
+const props = defineProps<{
+  route: string
+}>()
+
+const breadcrumbItems = ref<BreadcrumbItem[]>([
+  { text: 'Home', to: { path: '/' } },
+  { text: props.route },
+])
+</script>
+
+<template>
+  <BBreadcrumb :items="breadcrumbItems" class="m-3" />
+</template>
