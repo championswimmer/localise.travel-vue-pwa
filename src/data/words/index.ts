db95109c7c626dda24999b73af05dbd32a074a84
cgaswin <cgaswin333@gmail.com>
2024-12-27 21:28:59 +0530

    added spanish word list

--- src/data/words/index.ts
+++ src/data/words/index.ts
@@ -8,7 +8,10 @@ export interface TranslatedWords {
 } /** list of words in selected language */
 
 import { wordList as frenchWordList } from './fr'
+import { wordList as spanishWordList } from './esp'
+
 
 export const wordLists: { [lang: string]: TranslatedWords } = {
   fr: frenchWordList,
+  esp: spanishWordList
 }
