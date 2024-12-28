61a4fcbb20970b5ce0301a693d0e0456f718d9f1
Arnav Gupta <dev@championswimmer.in>
2024-12-28 02:01:38 +0000

    save language on localstorage
    Signed-off-by: Arnav Gupta <dev@championswimmer.in>

--- src/stores/lang.ts
+++ src/stores/lang.ts
@@ -2,16 +2,24 @@ import type { LangCode } from '@/data/languages'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useLangStore = defineStore('lang-select', () => {
-  const selector = ref<boolean>(false)
-  const toggleSelector = () => {
-    selector.value = !selector.value
-  }
+export const useLangStore = defineStore(
+  'lang-select',
+  () => {
+    const selector = ref<boolean>(false)
+    const toggleSelector = () => {
+      selector.value = !selector.value
+    }
 
-  const lang = ref<LangCode>('fr')
-  const setLang = (newLang: LangCode) => {
-    lang.value = newLang
-  }
+    const lang = ref<LangCode>('fr')
+    const setLang = (newLang: LangCode) => {
+      lang.value = newLang
+    }
 
-  return { lang, setLang, selector, toggleSelector }
-})
+    return { lang, setLang, selector, toggleSelector }
+  },
+  {
+    persist: {
+      pick: ['lang'],
+    },
+  },
+)
