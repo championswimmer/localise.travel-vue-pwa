975b908100b35f07cc18520c9b741ae2711f4b7b
Arnav Gupta <dev@championswimmer.in>
2024-12-28 01:18:47 +0000

    add lang selector and handle SPA redirects
    Signed-off-by: Arnav Gupta <dev@championswimmer.in>

--- src/stores/lang.ts
+++ src/stores/lang.ts
@@ -3,10 +3,15 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useLangStore = defineStore('lang-select', () => {
+  const selector = ref<boolean>(false)
+  const toggleSelector = () => {
+    selector.value = !selector.value
+  }
+
   const lang = ref<LangCode>('fr')
   const setLang = (newLang: LangCode) => {
     lang.value = newLang
   }
 
-  return { lang, setLang }
+  return { lang, setLang, selector, toggleSelector }
 })
