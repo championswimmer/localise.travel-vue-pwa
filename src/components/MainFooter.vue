48d2c0fad144f6317ecba549dbd350dc5671d3be
Akshaygore1 <akshaygore2301@gmail.com>
2025-01-02 00:17:25 +0530

    Footer fixed

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -12,42 +12,58 @@ import {
 <template>
   <footer class="footer mt-auto py-4 bg-light border-top">
     <div class="container">
-      <div class="row align-items-center">
-        <div class="col-md-auto fw-medium text-center text-md-start mb-2 mb-md-0">
-          Made with
-          <GitHubCopilotIcon :size="18" class="align-text-top text-primary mx-1" />
-          <VueDotjsIcon :size="18" class="align-text-top text-success mx-1" />
-          <BootstrapIcon :size="18" class="align-text-top text-info mx-1" />
-          <TypeScriptIcon :size="18" class="align-text-top text-blue mx-1" />
-          by
-          <a
-            href="https://championswimmer.in"
-            target="_blank"
-            class="fw-bold text-decoration-none text-dark"
+      <div class="row g-3">
+        <div class="col-12 col-md-6">
+          <div
+            class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-start gap-2"
           >
-            @championswimmer
-          </a>
+            <span>Made with</span>
+            <div class="d-flex align-items-center">
+              <GitHubCopilotIcon
+                style="width: 18px; height: 18px; margin: 0 4px; vertical-align: middle"
+              />
+              <VueDotjsIcon
+                style="width: 18px; height: 18px; margin: 0 4px; vertical-align: middle"
+              />
+              <BootstrapIcon
+                style="width: 18px; height: 18px; margin: 0 4px; vertical-align: middle"
+              />
+              <TypeScriptIcon
+                style="width: 18px; height: 18px; margin: 0 4px; vertical-align: middle"
+              />
+            </div>
+            <span>by</span>
+            <a
+              href="https://championswimmer.in"
+              target="_blank"
+              class="fw-bold text-decoration-none text-dark"
+            >
+              @championswimmer
+            </a>
+          </div>
         </div>
-        <div class="col-md-auto ms-md-auto text-center text-md-end">
-          <ul class="list-inline mb-0">
+        <div class="col-12 col-md-6">
+          <ul class="list-inline mb-0 d-flex justify-content-center justify-content-md-end gap-3">
             <li class="list-inline-item">
               <a
                 href="https://github.com/championswimmer/localise.travel-vue-pwa"
                 target="_blank"
-                class="text-decoration-none"
+                class="text-decoration-none text-dark d-inline-flex align-items-center"
               >
-                <GitHubIcon :size="18" class="align-text-top text-dark" />
-                <span class="ms-1">Source</span>
+                <GitHubIcon
+                  style="width: 18px; height: 18px; margin: 0 4px; vertical-align: middle"
+                />
+                <span>Source</span>
               </a>
             </li>
-            <li class="list-inline-item ms-3">
+            <li class="list-inline-item">
               <a
                 href="https://twitter.com/championswimmer"
-                class="text-decoration-none"
                 target="_blank"
+                class="text-decoration-none text-dark d-inline-flex align-items-center"
               >
-                <XIcon :size="18" class="align-text-top text-dark" />
-                <span class="ms-1">Contact</span>
+                <XIcon style="width: 18px; height: 18px; margin: 0 4px; vertical-align: middle" />
+                <span>Contact</span>
               </a>
             </li>
           </ul>
