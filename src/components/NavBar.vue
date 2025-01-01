8f3b502143cc955a98a8ef72e663a75c8edcafd1
marknaman05 <marknaman05@gmail.com>
2025-01-01 22:22:39 +0530

    Fixed screen size issue Created a resize eventlistener for small screen sizes which hides the Language Name and only shows Emoji of flag.
    Signed-off-by: Arnav Gupta <dev@championswimmer.in>

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,13 +3,27 @@ import { AppLanguages } from '@/data/languages'
 import { useLangStore } from '@/stores/lang'
 import { BNavbar, BNavbarBrand, BNavbarNav, BNavForm, BNavItem } from 'bootstrap-vue-next'
 import { Globe, Languages, Footprints, SlidersHorizontal } from 'lucide-vue-next'
-import { computed } from 'vue'
+import { computed ,  ref, onMounted, onUnmounted} from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 const langStore = useLangStore()
 
 const currentLang = computed(() => AppLanguages[langStore.lang!!])
+
+const isMobile = ref(window.innerWidth < 768)
+
+const updateIsMobile = () => {
+  isMobile.value = window.innerWidth < 768
+}
+
+onMounted(() => {
+  window.addEventListener('resize', updateIsMobile)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateIsMobile)
+})
 </script>
 
 <template>
@@ -28,8 +42,8 @@ const currentLang = computed(() => AppLanguages[langStore.lang!!])
     <BNavForm class="d-flex">
       <button class="btn btn-outline-secondary border-0" @click="langStore.toggleSelector">
         {{ currentLang.emoji }}
-        <span class="fw-medium">{{ currentLang.name }}</span>
-        <span class="fw-light">({{ currentLang.name_en }})</span>
+        <span v-if="!isMobile" class="fw-medium">{{ currentLang.name }}</span>
+        <span v-if="!isMobile" class="fw-light">({{ currentLang.name_en }})</span>
       </button>
     </BNavForm>
   </BNavbar>
