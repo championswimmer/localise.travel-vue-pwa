02726fc72879138f01c40e37f49427fcba89f1d1
Akshaygore1 <akshaygore2301@gmail.com>
2025-01-02 01:02:20 +0530

    Navbar fixes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,23 +13,21 @@ const currentLang = computed(() => AppLanguages[langStore.lang!!])
 </script>
 
 <template>
-  <BNavbar toggleable="lg">
-    <BNavbarBrand to="/" class="fw-bold">
+  <BNavbar toggleable="lg" class="container-fluid">
+    <BNavbarBrand to="/" class="fw-bold d-flex align-items-center">
       <Languages class="me-2 mb-1 text-primary" :size="24" />
       <Globe class="me-2 mb-1" :size="24" />
       <Footprints class="me-2 mb-1 text-secondary" :size="24" />
       <span class="text-primary">localise</span>.<span class="text-secondary">travel</span>
     </BNavbarBrand>
-    <!-- <BNavbarNav>
-      <BNavItem to="/home">Home</BNavItem>
-      <BNavItem to="/about">About</BNavItem>
-      <BNavItem to="/contact">Contact</BNavItem>
-    </BNavbarNav> -->
     <BNavForm class="d-flex">
-      <button class="btn btn-outline-secondary border-0" @click="langStore.toggleSelector">
+      <button
+        class="btn btn-outline-secondary border-0 d-flex align-items-center"
+        @click="langStore.toggleSelector"
+      >
         {{ currentLang.emoji }}
-        <span class="fw-medium">{{ currentLang.name }}</span>
-        <span class="fw-light">({{ currentLang.name_en }})</span>
+        <span class="fw-medium ms-2 d-none d-sm-inline">{{ currentLang.name }}</span>
+        <span class="fw-light ms-1 d-none d-sm-inline">({{ currentLang.name_en }})</span>
       </button>
     </BNavForm>
   </BNavbar>
