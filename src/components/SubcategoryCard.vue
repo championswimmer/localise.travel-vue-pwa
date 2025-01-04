8bfd8c18b93a76b860488f1a1efa78e7511b0f7a
Sanket <12sanketkumbhar@gmail.com>
2025-01-04 12:28:18 +0530

    hover and press style for sub cateogry cards

--- src/components/SubcategoryCard.vue
+++ src/components/SubcategoryCard.vue
@@ -2,6 +2,10 @@
 import type { Subcategory } from '@/data/categories'
 import { BCardTitle } from 'bootstrap-vue-next'
 import LucideIcon from './LucideIcon.vue'
+import { ref } from 'vue'
+
+const hover = ref(false)
+const press = ref(false)
 
 const props = defineProps<{
   subcategory: Subcategory
@@ -9,7 +13,19 @@ const props = defineProps<{
 </script>
 
 <template>
-  <BCard>
+  <BCard
+    class="border-2"
+    @mouseover="hover = true"
+    @mouseleave="hover = press = false"
+    @mousedown="press = true"
+    @mouseup="press = false"
+    :class="{
+      'border-secondary': hover && !press,
+      shadow: hover && !press,
+      'border-primary': press,
+      'shadow-sm': press,
+    }"
+  >
     <BCardTitle class="fs-5 fw-semibold text-lowercase">
       <LucideIcon :name="subcategory.icon" :size="24" :width="2.5" />
       {{ subcategory.name }}
